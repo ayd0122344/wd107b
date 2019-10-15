0e354c8f36e2e539d783815c1c9881c44c583b5b
ayd0122344 <ycvs210@gmail.com>
2019-10-15 21:39:44 +0800

    4-4-2.js

--- 習題/4-1-1.js
+++ 習題/4-1-1.js
@@ -1,41 +1,25 @@
 function insert(c,num){
     for(i=0;i<c.length;i++){
-        if(num>c[i] && num<c[i+1])//兩數之間
+        if(num<c[i])//小於全部
         {
-            for(j=c.length;j>i;j--)
-            {
-                c[j]=c[j-1];
-            }
-            c[i+1]=num;
+            c.splice(i,0,num);
             break;
         }
-        if(num==c[i])//等於某數
+        else if(num>c[c.length-1])//大於全部
         {
-            for(j=c.length;j>i;j--)
-            {
-                c[j]=c[j-1];
-            }
-            c[i+1]=num;
+            c.splice(c.length,0,num);
             break;
         }
-        if (num>c[c.length-1])//大於全部的數
+        else if((num>c[i] && num<c[i+1]) || (num==c[i]))//兩數之間 || 等於某數
         {
-            c[c.length]=num;
+            c.splice(i+1,0,num);
             break;
         }
-        if (num<c[0])//小於全部的數
-        {
-            for(j=c.length;j>i;j--)
-            {
-                c[j]=c[j-1];
-            }
-            c[i]=num;
-            break;
-        }
+
     }
     return c;
 }
-console.log(insert([2,5,6,8,9,10,12,16,17,18,19,20],8));//等於
+console.log(insert([2,5,6,8,9],8));//等於
 console.log(insert([0,1,6,8,15],3));//兩數間
-console.log(insert([3,6,7,12,15],2));//小於
+console.log(insert([3,6,7,8,12,],2));//小於
 console.log(insert([3,6,7,12,15],17));//大於
--- 習題/4-1-2.js
+++ 習題/4-1-2.js
@@ -1,2 +1,48 @@
 //請用二分搜尋法搜尋一個已經排好序的陣列。
 //範例： find([ 1, 4, 5, 8, 9], 5) => 2 , 找不到時傳回 -1。
+function find(c,target){
+    var v=c.length;
+    var middle=0,start=0,end=v;
+    for(middle=Math.floor((start+end)/2);middle>=0;Math.floor((start+end)/2))
+    {
+        if(c[middle]==target)
+        {
+            break;
+        }
+        else if(c[middle]<target)
+        {
+            //console.log("<時"+middle);
+            start=middle+1;
+            if((start+end)/2 >= (start+end)%2)
+            {
+                middle=(start+end)/2-(start+end)%2;
+            }
+            else break;
+        }
+        else//(c[middle]>target)
+        {
+            //console.log(">時"+middle);
+            end=middle-1;
+            //console.log((start+end)/2);
+            //console.log((start+end)%2);
+            if((start+end)/2 >= (start+end)%2)
+            {
+                middle=(start+end)/2-(start+end)%2;
+            }
+            else break;
+        }
+     }
+    //console.log(middle);
+    if(c[middle]==target)
+        {
+            return middle;
+        }
+    else
+        {
+            return -1;
+        }
+}
+console.log(find([0,1,6,8,15],3));//找不到
+console.log(find([3,6,7,8,12,],2));//找不到
+console.log(find([2,5,6,7,8,9],8));//找到
+console.log(find([3,6,7,12,15],12));//找到
